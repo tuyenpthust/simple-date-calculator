d90a359084c397cf7f90f2e06a33198c64b66076
Tuyên Phan Thanh <tuyenpt.hust@gmail.com>
2024-03-13 22:36:50 +0700

    minor changes

--- test/test.mjs
+++ test/test.mjs
@@ -1,10 +1,10 @@
 import {calculateDate} from "simple-date-calculator";
 
 // day different = [{ dayDiff: 2 }]
-console.log(calculateDate("2022-04-15", "2022-04-17"));
+console.log(calculateDate("2024-04-15", "2024-04-17"));
 
 // day, month different = [{ dayDiff: 3, monthDiff: 2 }]
-console.log(calculateDate("2022-04-15", "2022-07-17"));
+console.log(calculateDate("2024-04-15", "2024-07-17"));
 
 
 // day, month, year different = [{ dayDiff: 8, monthDiff: 2, yearDiff: 1 }]
